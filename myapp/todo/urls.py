6b58e776d9c25b21273b2795397ffd5d03ea8dd1
sung2708 <aki18082002@gmail.com>
2024-03-28 17:58:52 +0700

    create model

--- myapp/todo/urls.py
+++ myapp/todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('update/<str:pk>/', views.updateTask, name='update'),
+    path('delete/<str:pk>/', views.deleteTask, name='delete'),
 ]
--- myapp/todo/views.py
+++ myapp/todo/views.py
@@ -1,5 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+from .models import Task
+from .forms import TaskForm
+
 def index(request):
-    return render(request, 'todo/home.html');
+    task = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    
+    context = {'task': task, 'form': form}
+    return render(request, 'todo/home.html', context)
+
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    
+    context = {'form': form}
+    return render(request, 'todo/update.html', context)
+
+def deleteTask(request, pk):
+    item = Task.objects.get(id=pk)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('/')
+    
+    context = {'item':item}   
+    return render(request, 'todo/delete.html', context)
